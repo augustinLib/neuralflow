7552e04b9368edd9332d06314c1e069250cecf29
augustinLib <2bambitious@gmail.com>
2023-01-06 03:45:06 +0900

    Feat: Add trainer

--- simpleDL/trainer.py
+++ simpleDL/trainer.py
@@ -1,4 +1,6 @@
 from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 import numpy as np
 
 
@@ -6,6 +8,7 @@ class BaseTrainer():
     def __init__(self, n_epochs= 10, init_lr=0.01):
         self.n_epochs = n_epochs
         self.init_lr = init_lr
+        self.metric = OrderedDict()
         self.train_loss_list = np.array([])
         self.valid_loss_list = np.array([])
 
@@ -23,8 +26,8 @@ class ClassificationTrainer(BaseTrainer):
         self.optimizer = optimizer
         self.is_validate = is_validate
 
-        self.train_accuarcy_list = np.array([])
-        self.valid_accuarcy_list = np.array([])
+        self.train_accuracy_list = np.array([])
+        self.valid_accuracy_list = np.array([])
 
 
     def _forward(self, x, y):
@@ -45,11 +48,11 @@ class ClassificationTrainer(BaseTrainer):
     
 
     def train(self, train_dataloader, valid_dataloader = None):
-        for epoch in self.n_epochs:
+        for epoch in range(self.n_epochs):
             print(f"epoch {epoch+1}")
             tmp_train_loss = np.array([])
             train_correct_num = 0
-
+            y_num = 0
             for x, y in tqdm(train_dataloader):
                 train_loss = self._forward(x, y)
                 tmp_train_loss = np.append(tmp_train_loss, train_loss)
@@ -58,6 +61,9 @@ class ClassificationTrainer(BaseTrainer):
                 if y.ndim != 1:
                     y = np.argmax(y, axis=1)
                 train_correct_num += np.sum(pred==y)
+                y_num += len(y)
+
+                print(f"train loss : {train_loss}    train accuarcy : {train_correct_num/y_num*100}\r", end="")
 
                 _ = self._backward()
                 self._update()
@@ -65,23 +71,25 @@ class ClassificationTrainer(BaseTrainer):
                 
             epoch_train_loss = np.sum(tmp_train_loss)
             self.train_loss_list = np.append(self.train_loss_list, epoch_train_loss)
-
-            epoch_train_accuracy = float(train_correct_num)/len(train_dataloader)
-            self.train_accuarcy_list = np.append(self.train_accuarcy_list, epoch_train_accuracy)
-
-            print(f"train l")
-            print("------------------")    
+            dataset_len = train_dataloader.dataset_len()
+            epoch_train_accuracy = train_correct_num/float(dataset_len) * 100
+            self.train_accuracy_list = np.append(self.train_accuracy_list, epoch_train_accuracy)
+            print()
+            print(f"epoch {epoch+1} -- train loss : {epoch_train_loss}    train accuarcy : {epoch_train_accuracy}")
+            
             
             if valid_dataloader is not None:
-                self._validate(valid_dataloader)
+                self._validate(valid_dataloader, epoch)
+            else:
+                print("--------------------------------")    
 
 
 
 
-    def _validate(self, valid_dataloader):
+    def _validate(self, valid_dataloader, epoch):
         tmp_valid_loss = np.array([])
         valid_correct_num = 0
-
+        y_num = 0
         for x, y in tqdm(valid_dataloader):
             valid_loss = self._forward(x, y)
             tmp_valid_loss = np.append(tmp_valid_loss, valid_loss)
@@ -89,19 +97,43 @@ class ClassificationTrainer(BaseTrainer):
             if y.ndim != 1:
                 y = np.argmax(y, axis=1)
             valid_correct_num += np.sum(pred==y)
+            y_num += len(y)
+
+            print(f"valid loss : {valid_loss}    valid accuarcy : {valid_correct_num/y_num*100}\r", end="")
 
         epoch_valid_loss = np.sum(tmp_valid_loss)
         self.valid_loss_list = np.append(self.valid_loss_list, epoch_valid_loss)
+        dataset_len = valid_dataloader.dataset_len()
+        epoch_valid_accuracy = valid_correct_num/float(dataset_len) * 100
+        self.valid_accuracy_list = np.append(self.valid_accuracy_list, epoch_valid_accuracy)
+        print()
+        print(f"epoch {epoch+1} -- valid loss : {epoch_valid_loss}    valid accuarcy : {epoch_valid_accuracy}")
+        print("--------------------------------")    
 
-        epoch_valid_accuracy = float(valid_correct_num)/len(valid_dataloader)
-        self.valid_accuarcy_list = np.append(self.valid_accuarcy_list, epoch_valid_accuracy)
+
+    def show_error_graph(self):
+        plt.title("train/valid loss per epoch")
+        plt.plot(self.train_loss_list, "-r" ,label = 'train error')
+        plt.plot(self.valid_loss_list, "-b" ,label = 'valid error')
+
+        plt.legend(loc="upper right")
+
+        plt.grid()
+        plt.show()
+
+
+    def show_accuracy_graph(self):
+        plt.title("train/valid accuarcy per epoch")
+        plt.plot(self.train_accuracy_list, "-r" ,label = 'train accuracy')
+        plt.plot(self.valid_accuracy_list, "-b" ,label = 'valid accuracy')
+
+        plt.legend(loc="lower right")
+
+        plt.grid()
+        plt.show()
 
 
 
-
-
-
-    
     def add_metric(self, **metric):
         print(f"metic {metric.keys()} added")
         self.metric = dict(self.metric, metric)
