8a5bbc88ab63f73537a6f155f67d9089c3a06981
augustinLib <2bambitious@gmail.com>
2023-01-21 15:45:33 +0900

    Feat: Add LMtrainer

--- simpleDL/trainer.py
+++ simpleDL/trainer.py
@@ -1,8 +1,14 @@
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from collections import OrderedDict
-import numpy as np
+from simpleDL.gpu import set_device
 
+config = set_device("cpu")
+
+if config == "cpu":
+    import numpy as np
+else:
+    import cupy as np
 
 
 class BaseTrainer():
@@ -24,9 +30,8 @@ class BaseTrainer():
         self.valid_loss_list = np.array([])
 
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return "Trainer"
-
 
 
 class ClassificationTrainer(BaseTrainer):
@@ -64,16 +69,14 @@ class ClassificationTrainer(BaseTrainer):
 
     
     def _backward(self):
-        self.model._backward(self.critic)
-        grad = self.model.gradient()
-        return grad
-
+        self.model.backward(self.critic)
+        
     
     def _update(self):
         self.optimizer.update(self.model)
     
 
-    def train(self, train_dataloader, valid_dataloader = None) -> None:
+    def train(self, train_dataloader, valid_dataloader = None):
         """
         Train model with train/valid data
 
@@ -101,7 +104,7 @@ class ClassificationTrainer(BaseTrainer):
 
                 print(f"train loss : {train_loss}    train accuarcy : {train_correct_num/y_num*100}\r", end="")
 
-                _ = self._backward()
+                self._backward()
                 self._update()
             
                 
@@ -170,6 +173,153 @@ class ClassificationTrainer(BaseTrainer):
         plt.plot(self.valid_accuracy_list, "-b" ,label = 'valid accuracy')
 
         plt.legend(loc="lower right")
+
+        plt.grid()
+        plt.show()
+
+
+    def add_metric(self, **metric):
+        print(f"metic {metric.keys()} added")
+        self.metric = dict(self.metric, metric)
+        
+        
+class LanguageModelTrainer(BaseTrainer):
+    def __init__(self, model, critic, optimizer, n_epochs= 10, init_lr=0.01):
+        """
+        Trainer for Language modeling
+
+        Parameters
+        ----------
+        model : model for classification task
+
+        critic : loss function class
+
+        optimizer : optimizer for model optimizing
+
+        n_epochs (int) : number of epochs. Default: 10
+
+        init_lr (int) : initial learning rate. Default: 0.01
+
+        """
+        super().__init__(n_epochs, init_lr)
+        self.model = model
+        self.critic = critic
+        self.optimizer = optimizer
+
+        self.train_perplexity_list = np.array([])
+        self.valid_perplexity_list = np.array([])
+
+
+    def _forward(self, x, y):
+        pred = self.model(x)
+        loss = self.critic(pred, y)
+
+        return loss
+
+    
+    def _backward(self):
+        self.model.backward(self.critic)
+        
+    
+    def _update(self):
+        self.optimizer.update(self.model)
+    
+
+    def train(self, train_dataloader, valid_dataloader = None):
+        """
+        Train model with train/valid data
+
+        Parameters
+        ----------
+        train_dataloader (iterable) : Train dataloader that can iterate with batch size in train dataset
+
+        valid_dataloader (iterable) : Valid dataloader that can iterate with batch size in valid dataset
+
+        """
+        for epoch in range(self.n_epochs):
+            print(f"epoch {epoch+1}")
+            tmp_train_loss = np.array([])
+            tmp_train_perplexity = np.array([])
+            count = 0
+            for x, y in tqdm(train_dataloader):
+                count +=1
+                train_loss = self._forward(x, y)
+                tmp_train_loss = np.append(tmp_train_loss, train_loss)
+                train_perplexity = np.exp(train_loss)
+                tmp_train_perplexity = np.append(tmp_train_perplexity, train_perplexity)
+
+                print(f"train loss : {train_loss}    train perplexity : {train_perplexity}\r", end="")
+
+                self._backward()
+                self._update()
+            
+            epoch_train_loss = np.sum(tmp_train_loss) / count
+            self.train_loss_list = np.append(self.train_loss_list, epoch_train_loss)
+            epoch_train_perplexity = np.sum(tmp_train_perplexity) / count
+            self.train_perplexity_list = np.append(self.train_perplexity_list, epoch_train_perplexity)
+            
+            print()
+            print(f"epoch {epoch+1} -- train loss : {epoch_train_loss}    train perplexity : {epoch_train_perplexity}")
+            
+            
+            if valid_dataloader is not None:
+                self._validate(valid_dataloader, epoch)
+            else:
+                print("--------------------------------")    
+
+
+    def _validate(self, valid_dataloader, epoch):
+        tmp_valid_loss = np.array([])
+        tmp_valid_perplexity = np.array([])
+        count = 0
+        for x, y in tqdm(valid_dataloader):
+            count +=1
+            valid_loss = self._forward(x, y)
+            tmp_valid_loss = np.append(tmp_valid_loss, valid_loss)
+            valid_perplexity = np.exp(valid_loss)
+            tmp_valid_perplexity = np.append(tmp_valid_perplexity, valid_perplexity)
+            print(f"train loss : {valid_loss}    train perplexity : {valid_perplexity}\r", end="")
+        epoch_valid_loss = np.sum(tmp_valid_loss) / count
+        self.valid_loss_list = np.append(self.valid_loss_list, epoch_valid_loss)
+        epoch_valid_perplexity = np.sum(tmp_valid_perplexity) / count
+        self.valid_perplexity_list = np.append(self.valid_perplexity_list, epoch_valid_perplexity)
+        
+        print()
+        print(f"epoch {epoch+1} -- valid loss : {epoch_valid_loss}    valid accuarcy : {epoch_valid_perplexity}")
+        print("--------------------------------")    
+
+
+    def show_error_graph(self, valid = False):
+        """
+        Visualize training/validation error
+        """
+        if valid:
+            plt.plot(self.valid_loss_list, "-b" ,label = 'valid error')
+            plt.title("train/valid loss per epoch")
+        else:
+            plt.title("train loss per epoch")
+            
+        plt.plot(self.train_loss_list, "-r" ,label = 'train error')
+
+        plt.legend(loc="upper right")
+
+        plt.grid()
+        plt.show()
+
+
+    def show_perplexity_graph(self, valid = False):
+        """
+        Visualize training/validation perplexity
+        """
+        if valid:
+            plt.plot(self.valid_loss_list, "-b" ,label = 'valid perplexity')
+            plt.title("train/valid perplexity per epoch")
+        else:
+            plt.title("train perplexity per epoch")
+            
+        plt.plot(self.train_loss_list, "-r" ,label = 'train perplexity')
+
+        plt.legend(loc="upper right")
 
         plt.grid()
         plt.show()
